0ed7dede8f2aca903adbec474ff9720d81cfd37d
Deyvison Silva <deyvisondasilva@gmail.com>
2023-03-16 11:58:25 -0300

    First node file and create a first project node and installing packages node

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro arquivo Javascript no Node!");
+
+const numero1 = 2;
+const numero2 = 2;
+
+const soma = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log("O resultado da some é: " + soma(numero1, numero2));
---
+++ primeiro-projeto/primeiro-arquivo.ts
@@ -0,0 +1,9 @@
+console.log("Meu primeiro arquivo typescript!");
+
+let numero1: number = 5;
+let numero2: number = 10;
+
+const resultadoSoma: string = "O resultado da soma é: ";
+const soma = numero1 + numero2;
+
+console.log(resultadoSoma + soma);
